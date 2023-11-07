356fd5c32c7b506f44281f8b6aac4a35475fbab1
kk3939 <kyo51310@gmail.com>
2023-11-07 21:38:17 +0900

    setup lint and so on

--- App.tsx
+++ App.tsx
@@ -1,25 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="alertを出す" onPress={() => Alert.alert("test", "message", [
-      {
-        text: 'Cancel',
-        onPress: () => Alert.alert('Cancel Pressed'),
-        style: 'cancel',
-      },
-    ],
-    {
-      cancelable: true,
-      // only works on Android
-      // onDismiss: () =>
-      //   Alert.alert(
-      //     'This alert was dismissed by tapping outside of the alert dialog.',
-      //   ),
-    })}/>
+      <Button
+        title="alertを出す"
+        onPress={() =>
+          Alert.alert(
+            "test",
+            "message",
+            [
+              {
+                text: "Cancel",
+                onPress: () => Alert.alert("Cancel Pressed"),
+                style: "cancel",
+              },
+            ],
+            {
+              cancelable: true,
+              // only works on Android
+              // onDismiss: () =>
+              //   Alert.alert(
+              //     'This alert was dismissed by tapping outside of the alert dialog.',
+              //   ),
+            },
+          )
+        }
+      />
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
@@ -28,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
