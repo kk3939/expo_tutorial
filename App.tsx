4329742b74ff76cb1c610399938799cb553fc3f5
kk3939 <kyo51310@gmail.com>
2023-11-08 22:13:36 +0900

    setup image

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
 /* eslint-disable react/style-prop-object */
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#FFF" }}>
-        Open up App.js to start working on your app!
-      </Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +27,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Expo React Native App Tutorial
 https://docs.expo.dev/tutorial/create-your-first-app/
+
+The next is started from https://docs.expo.dev/tutorial/image-picker/ .
